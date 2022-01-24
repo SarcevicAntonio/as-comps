b5925f7befcde142946df17c8e0d6a2c52c6b1ee
Antonio Sarcevic <sarcevicantonio@gmailcom>
2022-01-25 00:09:37 +0100

    added positions

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,8 +1,9 @@
 <script>
 	import { Notifications } from '$lib';
 	import '../global.css';
+	import { notificationPosition } from './_notification/Demo.svelte';
 </script>
 
 <slot />
 
-<Notifications />
+<Notifications position={$notificationPosition} />
