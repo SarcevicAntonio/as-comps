cfed7798fcc78b0fbb1d51d20f74d8258aa59ade
Antonio Sarcevic <sarcevicantonio@gmailcom>
2022-02-16 23:55:34 +0100

    accordion mode for tabs and ARIA

--- src/lib/TabGroup/Tab.svelte
+++ src/lib/TabGroup/Tab.svelte
@@ -1,23 +1,83 @@
 <script>
 	import { getContext, onMount } from 'svelte';
 	import { key } from './TabGroup.svelte';
+	import { slide, fade } from 'svelte/transition';
+	import { v4 as uudi } from 'uuid';
+
+	let controlsId = uudi();
+	let labelledbyId = uudi();
 
 	export let label;
 
-	const { addTab, removeTab, activeTab } = getContext(key);
+	let tab = { label, controlsId, labelledbyId };
+
+	const { addTab, removeTab, activeTab, accordionStore, triggerClassStore } = getContext(key);
 
 	onMount(() => {
-		addTab(label);
+		addTab(tab);
 		return () => {
-			removeTab(label);
+			removeTab(tab);
 		};
 	});
+
+	let expanded = false;
 </script>
 
 <noscript>
-	<strong>{label}</strong>
+	<section {...$$restProps}>
+		<em>{label}</em>
+		<br />
+		<slot />
+	</section>
 </noscript>
 
-<section style="display: {!$activeTab || $activeTab === label ? 'block' : 'none'}" {...$$restProps}>
-	<slot />
-</section>
+{#if !$accordionStore}
+	{#if $activeTab === tab}
+		<section
+			in:fade
+			{...$$restProps}
+			role="tabpanel"
+			id={controlsId}
+			aria-labelledby={labelledbyId}
+		>
+			<slot />
+		</section>
+	{/if}
+{:else}
+	<section {...$$restProps}>
+		<div role="heading" id={labelledbyId}>
+			<button
+				on:click={() => {
+					expanded = !expanded;
+				}}
+				aria-expanded={expanded}
+				aria-controls={controlsId}
+				class:active={expanded}
+				class={$triggerClassStore}
+			>
+				{label}
+			</button>
+		</div>
+		{#if expanded}
+			<div transition:slide id={controlsId} aria-labelledby={labelledbyId} role="region">
+				<slot />
+			</div>
+		{/if}
+	</section>
+{/if}
+
+<style>
+	section[role='tabpanel'] {
+		margin-top: 1em;
+	}
+	div[role='heading'],
+	div[role='region'] {
+		margin-bottom: 1em;
+	}
+	button {
+		width: 100%;
+	}
+	.active {
+		text-decoration: underline;
+	}
+</style>
--- src/routes/_tabgroup/Demo.svelte
+++ src/routes/_tabgroup/Demo.svelte
@@ -1,12 +1,14 @@
 <script>
 	import Codesample from '$internal/Codesample.svelte';
 	import { Tab, TabGroup } from '$lib';
+
+	let accordion = false;
 </script>
 
 <section class="toybox">
 	<div class="demo">
 		<div class="card" style="width: 600px;">
-			<TabGroup triggerClass="btn">
+			<TabGroup triggerClass="btn" {accordion}>
 				<Tab label="Tab One">
 					<span data-test="tab-one-content">some random tab one content</span>
 				</Tab>
@@ -21,9 +23,18 @@
 			</TabGroup>
 		</div>
 	</div>
+	<div class="controls">
+		<label>
+			<span>Accordion</span>
+			<select bind:value={accordion}>
+				<option value={false}>false (default)</option>
+				<option value={true}>true</option>
+			</select>
+		</label>
+	</div>
 	<Codesample
 		code={`<div class="card" style="width: 600px;">
-<TabGroup triggerClass="btn">
+<TabGroup ${accordion ? 'accordion ' : ''}triggerClass="btn">
 	<Tab label="Tab One">some random tab content</Tab>
 	<Tab label="Second Tab">other tab content</Tab>
 	<Tab label="Tab, the Third">
