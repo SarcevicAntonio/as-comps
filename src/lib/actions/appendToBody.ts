2e5bcb977c7b5de03a32d0c1f8e92ab658a72756
Antonio Sarcevic <sarcevicantonio@gmail.com>
2022-05-02 23:24:25 +0200

    removed console.log

--- src/lib/actions/appendToBody.ts
+++ src/lib/actions/appendToBody.ts
@@ -1,7 +1,6 @@
 export default function appendToBody(node: HTMLElement): void {
 	const possibleParentCssProps = node.parentElement.style.cssText;
 	document.body.appendChild(node);
-	console.log(possibleParentCssProps);
 	if (possibleParentCssProps.startsWith('display: contents; --')) {
 		node.style.cssText = possibleParentCssProps.substring(19, possibleParentCssProps.length - 1);
 	}
